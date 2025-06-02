d5955c46288dc67d83a39bfc310e34ac47b828de
Mario Reder <mario.reder@pm.me>
2025-06-02 10:21:13 +0200

    try fix vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig(() => {
   const isProduction = process.env.NODE_ENV === "production";
-  const noExternal = [/^@orderly.*$/, "@uiw/react-split"];
+  const noExternal = [
+    /^@orderly.*$/,
+    "@uiw/react-split",
+    "@layerzerolabs/lz-solana-sdk-v2",
+  ];
   if (isProduction) {
     noExternal.push("ethers");
   }
